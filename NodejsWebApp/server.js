cc4e57f86eda67371e1016232ee7441ba4e122de
davidtambjss <david.tam@bjss.com>
2016-02-07 18:05:40 +0000

    First commit

---
+++ NodejsWebApp/server.js
@@ -0,0 +1,29 @@
+﻿
+
+var request = require('request');
+var http = require('http');
+var port = process.env.port || 1337;
+
+
+//var promise = Promise;
+
+var returnRate = function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            obj = JSON.parse(body);
+            rates = obj.rates;
+            startServer(rates);
+        }
+    };
+
+
+function startServer(rates){
+    http.createServer(function (req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('rate = ' + JSON.stringify(rates));
+
+    }
+    ).listen(port);
+
+}
+
+request('http://api.fixer.io/latest', returnRate);
